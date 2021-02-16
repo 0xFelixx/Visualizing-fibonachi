30f95ec61479e68674ccbfa121c99f3a26beb9ad
Idun420 <74796878+Idun420@users.noreply.github.com>
2021-02-16 15:14:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from PIL import Image, ImageDraw
 
-im = Image.new("RGBA", (4269,4269), (0,0,0,255)) # intialising the image class
+im = Image.new("RGBA", (500,250), (0,0,0,255)) # intialising the image class
 draw = ImageDraw.Draw(im) # Getting ready to draw fibonachi
 
 num = 16 # The amount of fibonachi numbers to visualise
 fibo1 = 0 # Used to caculate the fibonachi sequence
 fibo = 1 # Same as above
 angle = 90 # Used to keep track of the angle of the arc
+divNumY = 1.5 # Used to decide where the center will be
+divNumX = 3 # Same as above
 
-x1 = im.size[0] / 2 # These variables are used to keep track of the size and placement of each arc
-y1 = im.size[0] / 2
-x2 = im.size[0] / 2
-y2 = im.size[0] / 2
+x1 = im.size[0] / divNumX # Used to keep track of the size and placement of each arc
+y1 = im.size[1] / divNumY
+x2 = im.size[0] / divNumX
+y2 = im.size[1] / divNumY
 
 
 for i in range(num): # Looping over the amount of numbers to generate
@@ -24,8 +26,8 @@ for i in range(num): # Looping over the amount of numbers to generate
         (x1,y1,x2,y2), # Deciding the size and placement of the arc
         angle - 90, # Deciding the angle of the arc
         angle,
-        fill = (255,255,255), # Making it white
-        width = 10 # Making the arc wider to mach it with the size of the image class
+        fill = (0,255,0), # Making it green
+        width = 1 # Making the arc wider to mach it with the size of the image class
     )
     if i % 4 == 0: # Making it run every fourth time
         x1 -= fibo1 # Doing some math to make the arcs fit
